e2d3d903f8d6b0a38bf52b75700c11b1c12e1e36
Mohammad Khatib <mkhatib727@gmail.com>
2014-01-07 19:46:09 -0800

    da

--- utils/travis/deploy.sh
+++ utils/travis/deploy.sh
@@ -1,24 +1,3 @@
-if [[ "$TRAVIS_BRANCH" == "" ]]
-then
-  echo "TRAVIS_BRANCH variable is empty. This util is only to be used by Travis CI."
-  exit 1
-elif [[ "$TRAVIS_BRANCH" != "master" ]]
-then
-  echo "Not master. No Deploy to do."
-  exit 0
-else
-  echo "On master. Deploying now..."
-  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-  git remote add web-client-heroku git@heroku.com:yo-trial-web-client.git
-  echo "Host heroku.com" >> ~/.ssh/config
-  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
-  echo "   CheckHostIP no" >> ~/.ssh/config
-  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
-  heroku keys:clear
-  yes | heroku keys:add
-  yes | git push web-client-heroku `git subtree split --prefix web-client/dist master`:master --force
-fi
-
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]
 then
   echo "This is a pull request. No deployment will be done."
